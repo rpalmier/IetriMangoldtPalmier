41bc007c551847a3ddf08ba17ac21d9c29840714
Ronald Palmier <rpalmier@gmail.com>
2024-11-01 20:40:03 -0300

    + Funca

--- src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/IetriMangoldtPalmierApplication.java
+++ src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/IetriMangoldtPalmierApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"model"})
-@EnableJpaRepositories("repository")
 public class IetriMangoldtPalmierApplication {
 
 	public static void main(String[] args) {
