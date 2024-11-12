d8bbd081b29e9a62c11add873b78db64b869132e
Ronald Palmier <rpalmier@gmail.com>
2024-11-12 15:08:32 -0300

    +Avances

--- src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/controller/CaminoController.java
+++ src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/controller/CaminoController.java
@@ -5,15 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desi.IetriMangoldtPalmier.IetriMangoldtPalmier.forms.CaminosVecinosForm;
 import com.desi.IetriMangoldtPalmier.IetriMangoldtPalmier.model.Camino;
 import com.desi.IetriMangoldtPalmier.IetriMangoldtPalmier.repository.CaminoRepository;
 import com.desi.IetriMangoldtPalmier.IetriMangoldtPalmier.service.CaminoService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/caminos")
@@ -25,21 +33,38 @@ public class CaminoController {
         this.caminoService = caminoService;
     }
     
-    @GetMapping("")
-    public String index() {
+    @RequestMapping(method=RequestMethod.GET)
+    public String index(Model modelo) {
+    	CaminosVecinosForm camino = new CaminosVecinosForm();
+    	modelo.addAttribute("formBean", camino);
     	return "camino";
     }
     
 
-    @GetMapping("/vecinos")
-    public String obtenerCaminosVecinos(@RequestParam Integer codigoPostal, ModelMap modelo) {
-        try {
-            List<Camino> caminos = caminoService.getCaminosByCiudadOrigen(codigoPostal);
-            modelo.addAttribute("caminos", caminos);
-            return "camino";
-        } catch (IllegalArgumentException e) {;
-        	
-        }
+    @RequestMapping(method=RequestMethod.POST)
+    public String obtenerCaminosVecinos( @ModelAttribute("formBean") @Valid CaminosVecinosForm  formBean,BindingResult result, ModelMap modelo,@RequestParam String action) {
+    	if(action.equals("Buscar"))
+    	{
+    		if(result.hasErrors()) {
+    			modelo.addAttribute("formBean",formBean);
+    			return "camino";
+    		}
+	    	try {
+	            List<Camino> caminos = caminoService.getCaminosByCiudadOrigen(formBean.getCodpost());
+	            modelo.addAttribute("caminos", caminos);
+	            return "camino";
+	        } catch (IllegalArgumentException e) {;
+		        ObjectError error = new ObjectError("globalError", e.getMessage());
+	            result.addError(error);
+	        }
+    	}
+    	if(action.equals("Cancelar"))
+    	{
+    		modelo.clear();
+    		return "redirect:/caminos";
+    	}
         return "camino";                   
     }
+    
+    
 }
--- src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/model/Camino.java
+++ src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/model/Camino.java
@@ -7,8 +7,9 @@ import jakarta.persistence.*;
 public class Camino {
 
     @Id
-    @Column(name = "id_camino", length = 10)
-    private Integer idCamino;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", length = 10)
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "ciudad_origen")
@@ -23,6 +24,46 @@ public class Camino {
 
     @Column(name = "estado_clima", length = 50)
     private String estadoClima;
+    
+    public Integer getId() {
+    	return id;
+    }
+    
+    public void setId (Integer _id) {
+    	id = _id;
+    }
+    
+    public Ciudad getCiudadOrigen() {
+    	return ciudadOrigen;
+    }
+    
+    public void setCiudadOrigen(Ciudad _ciudadOrigen) {
+    	ciudadOrigen = _ciudadOrigen;
+    }
+    
+    public Ciudad getCiudadDestino() {
+    	return ciudadDestino ;
+    }
+    
+    public void setCiudadDestino(Ciudad _ciudadDestino) {
+    	ciudadDestino = _ciudadDestino;
+    }
 
+    public String getEstadoCamino() {
+    	return estadoCamino ;
+    }
+    
+    public void setEstadoCamino(String _estadoCamino) {
+    	estadoCamino = _estadoCamino;
+    }
+    
+    public String getEstadoClima() {
+    	return estadoClima ;
+    }
+    
+    public void setEstadoClima(String _estadoClima) {
+    	estadoClima = _estadoClima;
+    }
+    
    }
 
