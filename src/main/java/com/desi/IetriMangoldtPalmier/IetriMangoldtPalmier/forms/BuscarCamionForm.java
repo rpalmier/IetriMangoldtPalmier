7c75d94d4ec498544d3fe47d0cb3b50aab601f19
Ronald Palmier <rpalmier@gmail.com>
2024-11-20 22:27:42 -0300

    + Fixes Ronald

--- src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/forms/BuscarCamionForm.java
+++ src/main/java/com/desi/IetriMangoldtPalmier/IetriMangoldtPalmier/forms/BuscarCamionForm.java
@@ -16,22 +16,3 @@ public class BuscarCamionForm {
         this.patente = patente;
     }
 }
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
-public class FlotaEnCiudadForm {
-
-    @NotBlank(message = "El código postal no puede estar vacío")
-    @Pattern(regexp = "\\d{4,8}", message = "El código postal debe tener 4 dígitos")
-    private String codigoPostal;
-
-    // Getters y setters
-    public String getCodigoPostal() {
-        return codigoPostal;
-    }
-
-    public void setCodigoPostal(String codigoPostal) {
-        this.codigoPostal = codigoPostal;
-    }
-}
